5ae513cdba6d0a422dd047cf67fa82ec0177194a
nicolobartolini <s1098210@studenti.univpm.it>
2022-01-06 11:03:10 +0100

    Correzione alla classe Temperatura

--- src/main/java/it/univpm/models/Temperatura.java
+++ src/main/java/it/univpm/models/Temperatura.java
@@ -2,8 +2,10 @@ package it.univpm.models;
 
 public class Temperatura {
 
-    private int valoreReale;
-    private int valorePercepito;
+    private double valoreReale;
+    private double valorePercepito;
+    private double valoreMinimo;
+    private double valoreMassimo;
     private Data data;
 
     /**
@@ -12,9 +14,11 @@ public class Temperatura {
      * @param valorePercepito Temperatura percepita.
      * @param dataEpoch Data nel formato "Epoch".
      */
-    public Temperatura(int valoreReale, int valorePercepito, long dataEpoch) {
+    public Temperatura(double valoreReale, double valorePercepito, double valoreMinimo, double valoreMassimo, long dataEpoch) {
         this.valoreReale = valoreReale;
         this.valorePercepito = valorePercepito;
+        this.valoreMinimo = valoreMinimo;
+        this.valoreMassimo = valoreMassimo;
         this.data = new Data(dataEpoch);
     }
 
@@ -22,14 +26,14 @@ public class Temperatura {
      * <b>Getter</b> dell'attributo <code>valoreReale</code>.
      * @return int valoreReale.
      */
-    public int getValoreReale() {
+    public double getValoreReale() {
         return valoreReale;
     }
     /**
      * <b>Setter</b> dell'attributo <code>valoreReale</code>.
      * @param valoreReale Temperatura reale.
      */
-    public void setValoreReale(int valoreReale) {
+    public void setValoreReale(double valoreReale) {
         this.valoreReale = valoreReale;
     }
 
@@ -37,15 +41,43 @@ public class Temperatura {
      * <b>Getter</b> dell'attributo <code>valorePercepito</code>.
      * @return int valorePercepito.
      */
-    public int getValorePercepito() {
+    public double getValorePercepito() {
         return valorePercepito;
     }
     /**
      * <b>Setter</b> dell'attributo <code>valorePercepito</code>.
      * @param valorePercepito Temperatura percepita.
      */
-    public void setValorePercepito(int valorePercepito) {
+    public void setValorePercepito(double valorePercepito) {
         this.valorePercepito = valorePercepito;
+    }
+    /**
+     * <b>Getter</b> dell'attributo <code>valoreMinimo</code>.
+     * @return int valoreMinimo.
+     */
+    public double getValoreMinimo() {
+        return valoreMinimo;
+    }
+    /**
+     * <b>Setter</b> dell'attributo <code>valoreMinimo</code>.
+     * @param valoreMinimo Temperatura minima.
+     */
+    public void setValoreMinimo(double valoreMinimo) {
+        this.valoreMinimo = valoreMinimo;
+    }
+    /**
+     * <b>Getter</b> dell'attributo <code>valoreMassimo</code>.
+     * @return int valoreMassimo.
+     */
+    public double getValoreMassimo() {
+        return valoreMassimo;
+    }
+    /**
+     * <b>Setter</b> dell'attributo <code>valoreMassimo</code>.
+     * @param valoreMassimo Temperatura massima.
+     */
+    public void setValoreMassimo(double valoreMassimo) {
+        this.valoreMassimo = valoreMassimo;
     }
 
     /**
